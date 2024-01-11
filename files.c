12416f7c1dab6696bf0675f4493dd6aa3570d430
SoulMist666 <1581088963@qq.com>
2024-01-11 14:36:23 +0800

    第二次提交测试

--- files.c
+++ files.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 
 int main(){
-	printf("first push to git..."\n);
+	printf("first push to git1..."\n);
+	
+	printf("first push to git2..."\n);
+	printf("first push to git3..."\n);
+	printf("first push to git4..."\n);
+	printf("first push to git5..."\n);
+	printf("first push to git6..."\n);
+
 	return 0;
 }
